dac95cf122344c41411df11163f4846ef8fb5c07
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-07-28 09:57:33 +0700

    change SpringSecurity config

--- trudexpert/src/main/java/ru/trudexpert/server/config/SecurityConfiguration.java
+++ trudexpert/src/main/java/ru/trudexpert/server/config/SecurityConfiguration.java
@@ -1,18 +1,19 @@
 package ru.trudexpert.server.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration{
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
@@ -21,5 +22,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .sessionManagement().disable();
+
+        return http.build();
     }
 }
