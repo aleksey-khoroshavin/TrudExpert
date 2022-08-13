fae67173a199f1d4118ca3790ab4317e6dea42f7
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-08-13 18:04:28 +0700

    add group_service branch

--- course_service/src/main/java/ru/trudexpert/server/exception/SnilsAlreadyRegisteredException.java
+++
@@ -1,8 +0,0 @@
-package ru.trudexpert.server.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CREATED, reason = "Такой СНИЛС уже зарегистрирован в системе")
-public class SnilsAlreadyRegisteredException extends Exception{
-}
