2abc0e6f4c223b94439aac14e682cb81ecb3f534
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-08-28 18:44:20 +0700

    remove trash

--- organization_service/src/main/java/ru/trudexpert/server/exception/ListenerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.trudexpert.server.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Нет такого слушателя, чтобы выполнить данную операцию")
-public class ListenerNotFoundException extends Exception{
-}
--- organization_service/src/main/java/ru/trudexpert/server/repository/OrganizationAgentRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.trudexpert.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.trudexpert.server.entity.OrganizationAgent;
-
-@Repository
-public interface OrganizationAgentRepository extends JpaRepository<OrganizationAgent, Long> {
-}
