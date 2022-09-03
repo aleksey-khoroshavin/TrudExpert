94f156444b9637483a627518fa37c1408c5ca0ce
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-09-03 20:04:07 +0700

    init base for group service, change port, add script for relation tables, remove unnecessary code

--- group_service/src/main/java/ru/trudexpert/server/controller/GroupController.java
+++ group_service/src/main/java/ru/trudexpert/server/controller/GroupController.java
@@ -4,13 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.trudexpert.server.service.GroupService;
 
 @Controller
 @RequestMapping("/trudexpert/group")
 @RequiredArgsConstructor
 public class GroupController {
+
+    private final GroupService groupService;
+
     @GetMapping
-    public String openGroupPAge(){
+    public String openGroupPage(){
         return "groups";
     }
+
+    @GetMapping("/search")
+    public String openGroupListPage(){
+
+
+
+        return "/groups/group_search";
+    }
+
 }
