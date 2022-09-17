9e7d5a7d609358ed52b36ba156a3b76ab4435917
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-09-17 21:35:03 +0700

    create one monolith, reject microservices architecture

--- group_service/src/main/java/ru/trudexpert/server/exception/ListenerAlreadyRegisteredException.java
+++
@@ -1,8 +0,0 @@
-package ru.trudexpert.server.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.CREATED, reason = "Такой слушатель уже зарегистрирован в системе")
-public class ListenerAlreadyRegisteredException extends Exception{
-}
