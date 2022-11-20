c5e8b99149e815b7482b5fe18b8f47f56f2cee80
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-11-20 20:25:22 +0700

    TDEX-0008 - Добавить логирование ошибок в файл

--- src/main/java/ru/trudexpert/server/controller/MainController.java
+++ src/main/java/ru/trudexpert/server/controller/MainController.java
@@ -1,6 +1,7 @@
 package ru.trudexpert.server.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/trudexpert/main")
 @RequiredArgsConstructor
+@Log4j2
 public class MainController {
 
     @GetMapping
